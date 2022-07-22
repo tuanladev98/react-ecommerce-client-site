a5a766a0ad0e82e50d18723dc2123761a5432739
Le Anh Tuan <leanhtuan.th98@gmail.com>
2022-07-23 04:35:24 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-
-import 'react-toastify/dist/ReactToastify.css';
 
 import Cart from './pages/Cart';
 import Home from './pages/Home';
@@ -16,41 +13,35 @@ import Shop from './pages/Shop';
 import Register from './pages/Register';
 import Checkout from './pages/Checkout';
 
-import { socket, SocketContext } from './socket/socketContext';
-
 const App = () => {
   const CURRENT_USER = JSON.parse(localStorage.getItem('currentUser'));
 
   return (
-    <SocketContext.Provider value={socket}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/shop">
-            <Shop />
-          </Route>
-          <Route path="/product/:code">
-            <ProductDetail />
-          </Route>
-          <Route path="/cart">
-            {!CURRENT_USER ? <Redirect to="/login" /> : <Cart />}
-          </Route>
-          <Route path="/checkout">
-            {!CURRENT_USER ? <Redirect to="/login" /> : <Checkout />}
-          </Route>
-          <Route path="/login">
-            {CURRENT_USER ? <Redirect to="/" /> : <Login />}
-          </Route>
-          <Route path="/register">
-            {CURRENT_USER ? <Redirect to="/" /> : <Register />}
-          </Route>
-        </Switch>
-      </Router>
-
-      <ToastContainer position="bottom-right" newestOnTop autoClose={2000} />
-    </SocketContext.Provider>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/shop">
+          <Shop />
+        </Route>
+        <Route path="/product/:code">
+          <ProductDetail />
+        </Route>
+        <Route path="/cart">
+          {!CURRENT_USER ? <Redirect to="/login" /> : <Cart />}
+        </Route>
+        <Route path="/checkout">
+          {!CURRENT_USER ? <Redirect to="/login" /> : <Checkout />}
+        </Route>
+        <Route path="/login">
+          {CURRENT_USER ? <Redirect to="/" /> : <Login />}
+        </Route>
+        <Route path="/register">
+          {CURRENT_USER ? <Redirect to="/" /> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 import App from './App';
+
 import { store } from './redux/store';
+import { socket, SocketContext } from './socket/socketContext';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <SocketContext.Provider value={socket}>
+      <App />
+      <ToastContainer position="bottom-right" newestOnTop autoClose={1500} />
+    </SocketContext.Provider>
   </Provider>,
   document.getElementById('root')
 );
